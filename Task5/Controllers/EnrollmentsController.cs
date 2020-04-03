d5796242b69d0909ba7aa2bfd7d77e7530c73057
Vladyslav Kutsenko <s19183@pjwstk.edu.pl>
2020-04-03 14:19:39 +0300

    Add project files.

---
+++ Task5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task5.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController] //implicit model validation
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult EnrollStudent()
+        {
+
+            return Ok();
+        }
+    }
+}
